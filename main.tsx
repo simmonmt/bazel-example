d493dab995b8318de00a10757414c3c487b91619
Matthew Simmons <simmonmt@acm.org>
2021-04-25 09:35:10 -0400

    got react loading

--- main.tsx
+++ main.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import {EchoRequest} from './echo_pb';
 import {EchoServiceClient} from './echo_grpc_web_pb';
 
@@ -6,3 +8,8 @@ const echoService = new EchoServiceClient('http://nash.simmonmt.org:8080', null,
 console.log("hello world");
 console.log(new EchoRequest());
 console.log(echoService);
+
+ReactDOM.render(
+  <h1>Hello, world</h1>,
+  document.getElementById("root")
+);
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
 const commonjs = require('@rollup/plugin-commonjs');
 const {nodeResolve} = require('@rollup/plugin-node-resolve');
+const replace = require('@rollup/plugin-replace');
 
 module.exports = {
   plugins: [
     nodeResolve(),
     commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
   ],
 };
